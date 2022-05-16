124eb9569fa4d3b4d97a6cd6be58d5a228313581
Bartek Wrzalski <bartekwrzalski@gmail.com>
2022-05-16 19:32:41 +0200

    Added number of games

--- ai_vs_ai.py
+++ ai_vs_ai.py
@@ -1,9 +1,12 @@
+import time
+
 import pygame
 from checkers.constants import WIDTH, HEIGHT
 from checkers.game import Game
 
 
 FPS = 60
+GAMES = 10
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
@@ -11,6 +14,9 @@ pygame.display.set_caption('Checkers')
 def main():
     game = Game(WIN, mode='ivi')
 
+    for i in range(GAMES):
+        game.start_game()
+        time.sleep(3)
     pygame.quit()
 
 
--- user_vs_user.py
+++ user_vs_user.py
@@ -4,6 +4,7 @@ from checkers.game import Game
 
 
 FPS = 60
+GAMES = 10
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
@@ -20,23 +21,24 @@ def main():
     clock = pygame.time.Clock()
     game = Game(WIN)
 
-    while run:
-        clock.tick(FPS)
+    for i in range(GAMES):
+        game.start_game()
+        while run:
+            clock.tick(FPS)
 
-        if game.winner() is not None:
-            print(game.winner())
-            run = False
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if game.winner():
                 run = False
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                row, col = get_row_col_from_mouse(pos)
-                game.select(row, col)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
 
-        game.update()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    row, col = get_row_col_from_mouse(pos)
+                    game.select(row, col)
+
+            game.update()
 
     pygame.quit()
 
